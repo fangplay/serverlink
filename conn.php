c51883d8bdad87ff7b184201946e63fcab1fe2ab
FANGPLAY SIRIRAK <40180085+fangplay@users.noreply.github.com>
2018-07-19 09:52:49 +0700

    Beta3

--- conn.php
+++ conn.php
@@ -1,14 +1,16 @@
 <?php
-    $host = "10.20.5.59";
-    $username = "root";
-    $password = "";
-    $dbname = "alumni";
-    
-    $conn = mysql_connect($host,$username,$password,$dbname);
-    
-    if (!$conn){
-        echo ("Can't link database server.".mysqli_connect_error());
-    }else {
-        echo "Connection Completed";
-    }
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "alumni";
+
+	//Connection
+	$conn = mysqli_connect($servername,$username,$password,$dbname);
+	
+	//Check
+	if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+	} else {
+		echo "Connection Completed";
+	}
 ?>
